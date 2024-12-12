6d7562de6f779c27fc42b7c9c24996a4755e89d1
Kareem moore <moorekareem877@gmail.com>
2024-12-12 11:54:32 -0500

    FIXED

--- config/dbconn.js
+++ config/dbconn.js
@@ -1,5 +1,4 @@
 import {Sequelize}  from 'sequelize';
-<<<<<<< HEAD
 import dotenv from 'dotenv' ;
 dotenv.config();
 // add sequelize connection
@@ -12,22 +11,7 @@ const sequelize = new Sequelize(
         dialect: 'mysql',
         port: process.env.DB_PORT, 
     })
-=======
-import dotenv from 'dotenv';
-dotenv.config();
-// add sequelize connection
-const sequelize = new Sequelize(
-    process.env.DB_NAME, // Database name
-    process.env.DB_USER, // Database user
-    process.env.DB_PASSWORD, // Database password
-    {
-      host: process.env.DB_HOST, // Database host
-      dialect: 'mysql', // Use MySQL as the database dialect
-      port: process.env.DB_PORT, // Database port
-    }
-  );
->>>>>>> f75c9455100d065c38fb48b0cb64c19a4f096652
-    
+
     console.log("Database::connected",true);
 
     export default sequelize
